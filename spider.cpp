573eae4c34f80d50b4393c6f606481a647a861b3
Paulo Victor Pimentel Coelho <pvictorcoelho@hotmail.com>
2020-10-27 14:07:42 -0300

    spider

---
+++ spider.cpp
@@ -0,0 +1,90 @@
+/*
+ * spider.cpp
+ *
+ *  Created on: 23 de out. de 2020
+ *      Author: Paulo Victor
+ */
+
+#include <GL/glut.h>
+#include <math.h>
+
+GLint click = 0;
+GLdouble posx, posy;
+void move(){
+	movement +=
+}
+void spider_body(GLdouble x, GLdouble y, GLdouble radius, GLint stacks){
+	glBegin(GL_LINE_LOOP);
+	for(GLint i = 0; i<stacks; i++){
+		glColor3f(0, 0, 255);
+		GLdouble theta = (2.0f * 3.14159265359 * i)/stacks;
+		GLdouble border_x = radius*cosf(theta), border_y = radius*sinf(theta);
+		glVertex2f(x + border_x, y + border_y);
+	}
+	glEnd();
+}
+void display(){
+	glClear (GL_COLOR_BUFFER_BIT);
+
+	spider_body(posx,posy, 0.5, 10000);//imagem cresce muito
+	glScalef (1, 1, 0);
+	if(click==1){
+		//GLdouble pixelx = 220*(2.0/800.0) - 1, pixely = 1-220*(2.0/800.0);
+		//glPushMatrix();
+
+		spider_body(posx,posy, 0.5, 10000);//imagem cresce muito
+		glScalef (1, 1, 0);
+		//spider_body(pixelx,pixely, 0.5, 10000);//real, mal se mexe, quanto maior mais ele desce
+		//glPopMatrix();
+	}
+	glutSwapBuffers();;
+}
+void mouse(GLint button, GLint state, GLint x, GLint y){
+	switch(button){
+		case GLUT_LEFT_BUTTON:
+			posx=x, posy=y;
+			click = 1;
+			break;
+		//case GLUT_RIGHT_BUTTON:
+		//	click = 2;
+		//	break;
+	}
+	display();
+}
+
+void init(){
+	//define a cor do fundo da janela
+	click = 0;
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glOrtho(-1, 1, -1, 1, -1, 1);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	//definem o sistema de visualização; neste caso estão definindo o tipo de posição/projeção
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+}
+
+void reshape (int w, int h){
+  glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glTranslatef (0.0, 0.0, -5.0);
+}
+
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);            // Initialize GLUT
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowPosition(200,200);
+   glutInitWindowSize(800,800);
+   glutCreateWindow("Tarefa 2");
+   init();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutMouseFunc(mouse);
+   glutMainLoop();
+   return EXIT_SUCCESS;
+}
