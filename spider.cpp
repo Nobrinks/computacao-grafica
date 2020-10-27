920c46c02575bd9475cca8d90266f80f0d6d8600
Vinicius Sathler <Vinicius_sathler3@yahoo.com.br>
2020-10-27 15:20:45 -0300

    corrigi a função reshape

--- spider.cpp
+++ spider.cpp
@@ -27,7 +27,8 @@ pair<GLdouble, GLdouble> normalize_coordinates(GLdouble x, GLdouble y){
 	return make_pair((2.0/(double)length)*x -1, (-2.0/(double)height)*y +1);
 }
 
-void spider_body(pair<GLdouble,GLdouble> pos, GLdouble radius){
+void spider_body(GLdouble radius){
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBegin(GL_LINE_LOOP);
 	for(GLint i = 0; i<stacks; i++){
 		glColor3f(0, 0, 255);
@@ -37,22 +38,18 @@ void spider_body(pair<GLdouble,GLdouble> pos, GLdouble radius){
 	}
 	glEnd();
 }
+
 void display(){
 	glClear (GL_COLOR_BUFFER_BIT);
-
-	spider_body(pos, 0.5);//imagem cresce muito
+	//cout << pos.first << " " << pos.second << endl;
+	glPushMatrix();
 	glScalef (1, 1, 0);
-	if(click==1){
-		//GLdouble pixelx = 220*(2.0/800.0) - 1, pixely = 1-220*(2.0/800.0);
-		//glPushMatrix();
-
-		spider_body(pos, 0.5);//imagem cresce muito
-		glScalef (1, 1, 0);
-		//spider_body(pixelx,pixely, 0.5, 10000);//real, mal se mexe, quanto maior mais ele desce
-		//glPopMatrix();
-	}
-	glutSwapBuffers();;
+	glTranslated(pos.first, pos.second, 0.0);
+	spider_body(0.2);//imagem cresce muito
+  	glPopMatrix();
+  	glutSwapBuffers();
 }
+
 void mouse(GLint button, GLint state, GLint x, GLint y){
 	switch(button){
 		case GLUT_LEFT_BUTTON:
@@ -61,45 +58,30 @@ void mouse(GLint button, GLint state, GLint x, GLint y){
 			break;
 		default:
 			break;
-		//case GLUT_RIGHT_BUTTON:
-		//	click = 2;
-		//	break;
 	}
 	glutPostRedisplay();
 }
 
 void init(){
-	//define a cor do fundo da janela
+
 	click = 0;
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glOrtho(-1, 1, -1, 1, -1, 1);
-	//glClear(GL_COLOR_BUFFER_BIT);
-	//definem o sistema de visualiza��o; neste caso est�o definindo o tipo de posi��o/proje��o
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 }
 
-void reshape (int w, int h){
-  glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-  glMatrixMode (GL_PROJECTION);
-  glLoadIdentity ();
-  gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glTranslatef (0.0, 0.0, -5.0);
-}
 
 int main(int argc, char** argv) {
-   glutInit(&argc, argv);            // Initialize GLUT
+   glutInit(&argc, argv);      
    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
    glutInitWindowPosition(200,200);
    glutInitWindowSize(length,height);
    glutCreateWindow("Tarefa 2");
    init();
    glutDisplayFunc(display);
-   glutReshapeFunc(reshape);
    glutMouseFunc(mouse);
    glutMainLoop();
    return EXIT_SUCCESS;
