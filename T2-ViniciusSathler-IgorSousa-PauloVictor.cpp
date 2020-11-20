d0336cd26f24985d75e155a25c455d15c6c487be
Vinicius Sathler <Vinicius_sathler3@yahoo.com.br>
2020-11-20 17:08:09 -0300

    update1

--- T2-ViniciusSathler-IgorSousa-PauloVictor.cpp
+++ T2-ViniciusSathler-IgorSousa-PauloVictor.cpp
@@ -29,6 +29,47 @@ enum Pernas{
 	L1, L2, L3, L4, L5, L6, L7, L8
 };
 
-int main(){
-    
+using namespace std;
+
+Estados estado;
+int const height = 800, length = 800;
+GLdouble r_torax, r_abd, direcao;
+
+
+pair<GLdouble, GLdouble> normalizeCoordinates(GLdouble x, GLdouble y){
+	return make_pair((2.0/(double)length)*x -1, (-2.0/(double)height)*y +1);
+}
+
+void init(){
+	estado = Estados::P1;
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glOrtho(-1, 1, -1, 1, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void display(){
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+
+    glColor3f(BLACK);
+    //gluSphere(gluNewQuadric(), 100, 100, 25);
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);      
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(200,200);
+    glutInitWindowSize(length,height);
+    glutCreateWindow("Trabalho 2");
+    init();
+    glutDisplayFunc(display);
+    //glutMouseFunc(mouse);
+    //glutTimerFunc(50, update, 0);
+    glutMainLoop();
+    return EXIT_SUCCESS;
 }
