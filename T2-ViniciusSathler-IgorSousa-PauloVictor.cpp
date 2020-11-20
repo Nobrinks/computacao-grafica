d6e2f7148ec82e7f92eaa1849aab94e1d87ca13f
Vinicius Sathler <Vinicius_sathler3@yahoo.com.br>
2020-11-20 15:40:48 -0300

    arquivo inicial

---
+++ T2-ViniciusSathler-IgorSousa-PauloVictor.cpp
@@ -0,0 +1,34 @@
+/*
+ * spider.cpp
+ *
+ *  Created on: 23 de out. de 2020
+ *      Author: Paulo Victor, Vinicius Sathler, Igor Silva
+ */
+
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+#define BLUE 0, 0, 1
+#define RED 1, 0, 0
+#define GREEN 0, 1, 0
+#define CYAN 0, 1, 1
+#define YELLOW 1, 1, 0
+#define BLACK 0, 0, 0
+#define WHITE 1, 1, 1
+
+#define NOCOLOR -1
+
+enum Estados{
+	P1, P2, P3
+};
+//	L1	L5
+//	L2	L6
+//	L3	L7
+//	L4	L8
+enum Pernas{
+	L1, L2, L3, L4, L5, L6, L7, L8
+};
+
+int main(){
+    
+}
