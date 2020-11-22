6420e2bf4d3d9612b537d3e3aac468285d283c16
Vinicius Sathler <Vinicius_sathler3@yahoo.com.br>
2020-11-22 14:39:22 -0300

    patas implementadas

--- T2-ViniciusSathler-IgorSousa-PauloVictor.cpp
+++ T2-ViniciusSathler-IgorSousa-PauloVictor.cpp
@@ -5,10 +5,15 @@
  *      Author: Paulo Victor, Vinicius Sathler, Igor Silva
  */
 
-#include <GL/glut.h>
 #include <bits/stdc++.h>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+
 
 #define BLUE 0, 0, 1
 #define RED 1, 0, 0
@@ -31,11 +36,148 @@ enum Pernas{
 	L1, L2, L3, L4, L5, L6, L7, L8
 };
 
+enum Dados{
+	APOS, OPOS, INCPOS, L, INCART
+};
+
+GLdouble const apos = 30;
+
+GLdouble const Val[8][3][5] = {
+    //l1
+    {
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -60, 45, 0.25, 90
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l2
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -20, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l3
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, 20, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l4
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, 45, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l5
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -120, 45, 0.25, 90
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l6
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -160, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l7
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -200, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+	//l8
+	{
+		//p1
+		{
+			//APOS, OPOS, INCPOS, L, INCART
+			apos, -225, 45, 0.20, 60
+		},
+		//p2
+		{
+			0, 0, 0, 0, 0
+		},
+		//p3
+		{
+			0, 0, 0, 0, 0
+		}
+	},
+};
+
 using namespace std;
 
 Estados estado;
 int winHeight = 800, winWidth = 800, stacks = 500;
-GLdouble r_torax = 0.2, r_abd = 0.3, direcao;
+GLdouble r_torax = 0.2, r_abd = 0.3, direcao, h_spider = 0;
 
 pair<GLdouble, GLdouble> normalizeCoordinates(GLdouble x, GLdouble y){
 	return make_pair((2.0/(double)winHeight)*x -1, (-2.0/(double)winWidth)*y +1);
@@ -57,19 +199,64 @@ void init(){
 	glLoadIdentity();
 }
 
+void drawLeg(Pernas perna){
+    GLdouble l1 = Val[perna][estado][Dados::L];
+    GLdouble inc_articulacao = Val[perna][estado][Dados::INCART], inc_pos = Val[perna][estado][Dados::INCPOS];
+
+    GLdouble pos_alpha = Val[perna][estado][Dados::APOS], pos_omega = Val[perna][estado][Dados::OPOS], h_pos = (r_abd-r_torax) + r_torax*(1 - cos((90-pos_alpha)*M_PI/180.0));
+    GLdouble hl = l1*(sin(inc_pos*(M_PI/180.0)));
+    GLdouble bl = inc_pos + inc_articulacao - 90;
+    GLdouble l2 = (hl + h_pos)/(cos(bl*(M_PI/180.0)));
+
+    glPushMatrix();
+    glRotated(pos_omega, 0, 0, 1.0);
+    glRotated(pos_alpha, 0, 1.0, 0);
+    glTranslatef(-r_torax, 0, 0);
+
+    glRotated(-pos_alpha-inc_pos, 0, 1.0, 0);
+    glTranslated(-l1, 0, 0);
+    glRotated(180-inc_articulacao, 0, 1.0, 0);
+    glTranslated(-l2/2, 0, 0);
+    glPushMatrix();
+        glScaled(l2, 0.02, 0.02);
+        glutSolidCube(1.0);
+    glPopMatrix();
+    glTranslated(l2/2, 0, 0);
+    glRotated(-(180-inc_articulacao), 0, 1.0, 0);
+    glTranslated(l1/2, 0, 0);
+    glPushMatrix();
+        glScaled(l1, 0.02, 0.02);
+        glutSolidCube(1.0);
+    glPopMatrix();
+    glTranslated(l1/2, 0, 0);
+
+    glRotated(pos_alpha+inc_pos, 0, 1.0, 0);
+
+    glTranslated(r_torax, 0, 0);
+    glRotated(-pos_alpha, 0, 1.0, 0);
+    glRotated(-pos_omega, 0, 0, 1.0);
+    glPopMatrix();
+}
+
 void drawSpiderBody(){
     glPushMatrix();
         glEnable(GL_TEXTURE_2D);
         glBindTexture( GL_TEXTURE_2D, texID[0] );  // Bind texture #0 for use on the spider body.
         glColor3f(WHITE);
+       
+        //glTranslated(0, 0, -1);
+        //gluSphere(quadricObj, r_torax, 20, 20);
 
-        glTranslated(0, 0, -1);
-        glutSolidTeapot(0.2);
+        for(int i = Pernas::L1; i <= Pernas::L8; i++) drawLeg(Pernas(i));
+
+        glutWireSphere(r_torax, 20, 20);
 
         glTranslated(0, -r_abd-r_torax, 0);
         gluQuadricDrawStyle(quadricObj, GLU_FILL);
-        gluSphere(quadricObj, 0.3, 20, 20);
-        gluQuadricTexture(quadricObj, GL_TRUE);
+        glutWireSphere(r_abd, 20, 20);
+
+        //gluSphere(quadricObj, r_abd, 20, 20);
+        //gluQuadricTexture(quadricObj, GL_TRUE);
         
     glPopMatrix();
 
